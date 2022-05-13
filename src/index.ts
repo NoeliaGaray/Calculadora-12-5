9653fb849b7abfa858e0ead3f565126e934b70c8
NoeliaGaray <103612855+NoeliaGaray@users.noreply.github.com>
2022-05-12 21:46:27 -0300

    función obtenerResultado

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,19 @@ btnEnv.addEventListener("click", () => {
   let tercernúmero: number = Number(dato3.value);
   let suma: number = primernúmero + segundonúmero;
   let resta: number = primernúmero - segundonúmero;
+
+  function obtenerResultadoSuma(a:number, b:number):number{
+    let resultado:number = 0;
+    resultado = a+b;
+    return resultado;
+  }
+  
+  function obtenerResultadoResta(c:number, d:number):number{
+    let resultado:number = 0;
+    resultado = c-d;
+    return resultado;
+  }
+
   function dibujarGuiones(n: number) {
     let i: number;
     let linea: string = "-";
@@ -20,13 +33,13 @@ btnEnv.addEventListener("click", () => {
   switch (tercernúmero) {
     case 1:
       dibujarGuiones(40);
-      console.log("El resultado de la operación es: ", suma);
+      console.log("El resultado de la operación es: ", obtenerResultadoSuma (Number(dato1.value),Number(dato2.value));
       dibujarGuiones(40);
       break;
 
     case 2:
       dibujarGuiones(20);
-      console.log("El resultado de la operación es: ", resta);
+      console.log("El resultado de la operación es: ", obtenerResultadoResta(Number(dato1.value),Number(dato2.value));
       dibujarGuiones(20);
       break;
 
